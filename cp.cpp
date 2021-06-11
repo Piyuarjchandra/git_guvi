4fa2f99114e74d8c9a07f1c5c44deefcbfebd685
piyush chandra <piyush-chandra@user.github.com>
2021-06-11 10:34:43 +0530

    new cpp file

---
+++ cp.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define pb push_back
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        cout<< "'Hello World'"<<endl;
+    }
+    return 0;
+}
