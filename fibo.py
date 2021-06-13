7884a899a9aced917c2896c15ac139d41483b5f8
piyush chandra <piyush-chandra@user.github.com>
2021-06-13 16:47:55 +0530

    conflict in fibo.py

--- fibo.py
+++ fibo.py
@@ -4,12 +4,7 @@ Created on Thu Oct 15 10:43:23 2020
 
 @author: patryk
 """
-def fib(n):    # write Fibonacci series up to n
-    a, b = 0, 1
-    while b < n:
-        print(b, end=' ')
-        a, b = b, a+b
-    print()
+print(a,b,v)
 
 def fib2(n): # return Fibonacci series up to n all this conflicts
     print(n)
