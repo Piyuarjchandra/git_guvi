5f5012880a842bc7bb6cdc2e183846126df3bf5c
piyush chandra <piyush-chandra@user.github.com>
2021-06-13 12:57:43 +0530

    added new funct

---
+++ palin.py
@@ -0,0 +1,28 @@
+def palindrom(x):
+    pn=0
+    while(x):
+        r=x%10
+        x=x//10
+        pn=pn*10+r
+    return pn
+def checkpalin(y):
+    x=palindrom(y)
+    if(y==x):
+        return 1
+    else:
+        return 0
+a=int(input())
+y=palindrom(a)
+z=a+y
+j=checkpalin(z)
+if(j==1):
+   print(z)
+else:
+  while True:
+     y=palindrom(z)
+     z=y+z
+     j=checkpalin(z)
+     if(j==1):
+         print(z)
+         break
+    
