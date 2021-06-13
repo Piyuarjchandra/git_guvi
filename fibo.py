b77641a83c5cb96d9174bdad9525ac2b73022d57
piyush chandra <piyush-chandra@user.github.com>
2021-06-13 16:42:16 +0530

    conflict

--- fibo.py
+++ fibo.py
@@ -12,12 +12,7 @@ def fib(n):    # write Fibonacci series up to n
     print()
 
 def fib2(n): # return Fibonacci series up to n
-    result = []
-    a, b = 0, 1
-    while b < n:
-        result.append(b)
-        a, b = b, a+b
-    return result
+    print(n)
 if __name__ == "__main__":
     import sys
     fib(int(sys.argv[1]))
